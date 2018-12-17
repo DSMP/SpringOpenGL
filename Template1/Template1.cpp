7599cde25e0a411ac1418c7e7c06c638e957c18b
Damian Smug <smugd@synevo.pl>
2018-12-17 09:44:56 +0100

    add: draw hexagon

--- Template1/Template1.cpp
+++ Template1/Template1.cpp
@@ -8,33 +8,59 @@
 #include "include\GL\glut.h"
 
 
-void triangle(int a) {
-	glBegin(GL_POLYGON);// OpenGL’a state
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	glVertex3f(0, 0, -30);
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	glVertex3f(5, 0, -30);
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	glVertex3f(5, 5, -30);
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	glVertex3f(0, 5, -30);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-	glVertex3f(0, 0, -35);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-	glVertex3f(5, 0, -35);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-	glVertex3f(5, 5, -35);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-	glVertex3f(0, 5, -35);
-	glEnd();
+void hexagon(int a) {
+	glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+	  // Top face (y = 1.0f)
+	  // Define vertices in counter-clockwise (CCW) order with normal pointing out
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+
+	// Bottom face (y = -1.0f)
+	glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	// Front face  (z = 1.0f)
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+
+	// Back face (z = -1.0f)
+	glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+
+	// Left face (x = -1.0f)
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+
+	// Right face (x = 1.0f)
+	glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glEnd();  // End of drawing color-cube
 }
 void MyDisplay(void) {
 	// The new scene
 	glLoadIdentity();
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glRotatef(90, 1, 1, 0);
-	glTranslatef(50, -30, 0);
-	triangle(1);
+	glTranslatef(0, 0, -20);
+	glRotatef(90, 1, 1, 1);
+	hexagon(1);
 
 	// The end of scene
 	glFlush();//start processing buffered OpenGL routines
