8f61beee1882cd0a785e9c5ffa469097bc21bda5
Damian Smug <smugd@synevo.pl>
2019-01-23 14:51:56 +0100

    add: system placer as userName of remoteuser

--- Template1/Template1.cpp
+++ Template1/Template1.cpp
@@ -135,6 +135,12 @@ void Spring(int a) {
 		}
 	}
 	glEnd();
+
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, _textureId);
+
 	//QL_Quads
 	glEnable(GL_TEXTURE_2D);
 	for (int j = 0; j < 125; j++)
